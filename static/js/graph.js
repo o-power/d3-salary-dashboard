33888e4ea540f7d963bdda41a7f814ef2086922c
Ubuntu <ubuntu@ip-172-31-84-197.ec2.internal>
2019-07-07 20:52:25 +0000

    Added comments to graph.js

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,23 +1,58 @@
-//d3.csv("data/Salaries.csv",function(salaryData) {
-//    console.log(salaryData.rank);
-//});
-
-// wait for the data to be loaded before making the graphs
-//queue()
-//    .defer(d3.csv, "data/Salaries.csv")
-//    .await(makeGraphs);
 queue()
     .defer(d3.csv, "data/Salaries.csv")
     .await(makeGraphs);
 
 function makeGraphs(error, salaryData) {
+    // Crossfilter.js is a JavaScript plug-in used to slice and dice JavaScript arrays. T
     var ndx = crossfilter(salaryData);
+    
+    // size
+    //console.log(ndx.size());
     
     // salaryData is an array of objects
     // {"": "1", rank: "Prof", discipline: "B", yrs.since.phd: "19", yrs.service: "18", sex: "Male", salary: "139750"}
     // note that numbers are stored as strings
     //console.log(salaryData[0]);
     //console.log(salaryData[0].salary);
+    
+    // The forEach() method calls a function once for each element in an array, in order.
+    salaryData.forEach(function(d) {
+        d.salary = parseInt(d.salary);
+    });
+    
+    //console.log(salaryData[0]);
+    
+    show_gender_balance(ndx);
+    
+    dc.renderAll();
+}
+
+
+function show_gender_balance(ndx) {
+    // A Crossfilter dimension represents a property of the data, be it an existing property or a derived property. (
+    // same as var dim = ndx.dimension(function(d) {return d.sex;});
+    var dim = ndx.dimension(dc.pluck('sex'));
+    var group = dim.group();
+    
+    // top 3
+    //console.log(dim.top(3));
+    
+    // With the grouping operations, our result set is an array of groups where each group has a key (the thing we’re grouping by) and a value such as the count or sum.
+    //console.log(group.all());
+    //0: {key: "Female", value: 39}
+    //1: {key: "Male", value: 358}
+    
+    dc.barChart("#gender-balance")
+        .width(400)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Gender")
+        .yAxis().ticks(20);
 }
 
 
